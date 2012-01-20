4f6585c3391bfb05637415fcf765c3329c1db688
Nathan Dotz <nathan.dotz@gmail.com>
2012-01-20 18:40:50 -0500

    Add fixtures and plugin to load them

--- spec/ContinuousViewSpec.js
+++ spec/ContinuousViewSpec.js
@@ -1,8 +1,21 @@
 describe('ContinuousView', function () {
+
+             beforeEach(function () {
+                            loadFixtures("canvas.html");
+                        });
              
              it('exists on page load', function () {
                     expect(window.BackyardBrains).toBeDefined();
                     expect(window.BackyardBrains.ContinuousView).toBeDefined();
                 });
 
+             it('grabs the canvas context properly', function () {
+                    var canvas = document.getElementById('waveformCanvas');
+                    window.BackyardBrains.ContinuousView.setup(canvas);
+                    expect(context).toBeDefined();
+                });
+
+             it('creates the draw function', function () {
+                    expect(window.BackyardBrains.ContinuousView.draw).toBeDefined();
+                    });
          });
--- src/byb.js
+++ src/byb.js
@@ -11,7 +11,9 @@
 
     BackyardBrains.ContinuousView.setup = function (domCanvas) {
         canvas = domCanvas;
-        var context = canvas.getContext('2d');
+
+        // console.log(this);
+        context = canvas.getContext('2d');
 
         context.fillStyle = 'rgb(0,0,0)';
 
@@ -28,7 +30,7 @@
         draw(context);
     };
 
-    function draw (context) {
+    BackyardBrains.ContinuousView.draw = function (context) {
         context.clearRect(0, 0, width, height);
         drawXAxis(context);
         drawYAxis(context);
@@ -42,7 +44,9 @@
         timePassed = timePassed + .01;
         c = timePassed*Math.PI;
 		setTimeout(function () { draw(context); }, 10);
-    }
+    };
+
+    draw = BackyardBrains.ContinuousView.draw;
 
     function drawWave(c, context) {
         var x = c;
@@ -92,51 +96,3 @@
         context.beginPath();
     }
 })(window);
-
-/*
-var datapointsWidth = 8000;
-var datapointsHeight = 4000;
-
-
-
-function draw(samplesToDraw) {
-
-
-           // Fill backing rectangle with black.
-        ctx.fillStyle = "rgb(0,0,0)";
-        ctx.fillRect(0,0,width,height);
-
-        ctx.strokeStyle = "rgb(0,255,0)";
-        ctx.beginPath();
-        ctx.moveTo(0, samplesToDraw[0] * heightRatio - height/2);
-        for (var i = 1; i < samplesToDraw.length; i++) {
-            ctx.lineTo(i, samplesToDraw[i] * heightRatio + height/2);
-        }
-
-        ctx.stroke();
-    }
-}
-
-function generateSineArray(hz, maxAmplitude, len, offset) {
-    var sampleArray = new Array();
-    offset = typeof(offset) != 'undefined' ? offset : 0;
-    for (var i = 0; i < len; i++) {
-        sampleArray[i] = Math.sin(i*hz) * maxAmplitude;
-    }
-    return sampleArray;
-}
-
-function generateNoiseArray(maxAmplitude, length) {
-    var sampleArray = new Array();
-    for(var i = 0; i < length; i++) {
-        sampleArray[i] = Math.random() * maxAmplitude;
-    }
-    return sampleArray;
-}
-
-function setDatapointsSize() {
-    datapointsHeight = document.getElementById("datapointsHeight").value;
-    datapointsWidth = document.getElementById("datapointsWidth").value;
-    draw(generateSineArray(0.01, datapointsHeight/2, datapointsWidth, 0));
-}
-*/
