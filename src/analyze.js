b430a424e3ed9cb20191b763f0e51ac96fa341a5
Nathan Dotz <nathan.dotz@gmail.com>
2012-03-01 21:56:11 -0500

    make it pretty with the tickmarks ;)

--- src/analyze.js
+++ src/analyze.js
@@ -1,5 +1,10 @@
 (function (w) {
 
+  PCM_MIN = -32768;
+  PCM_MAX = 32767;
+  BACKGROUND_UI_COLOR = 'rgb(48,96,48)';
+  GREEN = 'rgb(0,255,0)';
+
   BackyardBrains.AnalyzeView = {
       context: false,
       height: 0,
@@ -23,6 +28,7 @@
     that.x_axis = that.height/2;
     
     that.context.fillStyle = 'rgb(0,0,0)';
+    that.context.fillRect(0,0,that.width,that.height);
     that.context.lineJoin = 'round';
     that.context.save();
   };
@@ -31,13 +37,38 @@
     if (!(audioData instanceof Array)) {
       throw "Not an array";
     }
+
+    drawTickmarks();
+    drawXAxis();
+
+    context.strokeStyle = GREEN;
     context = BackyardBrains.AnalyzeView.context;
     context.beginPath();
-    context.moveTo(0,0);
+    context.moveTo(0, BackyardBrains.AnalyzeView.x_axis);
     $.each(audioData, function (index, value) {
       context.lineTo(remapValue(index, 0, audioData.length, 0, BackyardBrains.AnalyzeView.width),
-                     remapValue(value, -32768, 32767, 0, BackyardBrains.AnalyzeView.height));
+                     remapValue(value, PCM_MIN * 1.5, PCM_MAX * 1.5, 0, BackyardBrains.AnalyzeView.height));
     });
     context.stroke();
   };
+
+  function drawXAxis() {
+    context = BackyardBrains.AnalyzeView.context;
+    context.strokeStyle = BACKGROUND_UI_COLOR;
+    context.beginPath();
+    context.moveTo(0, BackyardBrains.AnalyzeView.x_axis);
+    context.lineTo(BackyardBrains.AnalyzeView.width, BackyardBrains.AnalyzeView.x_axis);
+    context.stroke();
+  };
+
+  function drawTickmarks (context) {
+    context = BackyardBrains.AnalyzeView.context;
+    context.strokeStyle = BACKGROUND_UI_COLOR;
+        for (var i = 0; i < 9; i++) {
+            context.moveTo(0, i/8 * BackyardBrains.AnalyzeView.height);
+            context.lineTo(5, i/8 * BackyardBrains.AnalyzeView.height);
+            context.stroke();
+        }
+ }
+
 })(window);
