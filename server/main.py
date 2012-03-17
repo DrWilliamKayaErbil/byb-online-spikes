fb6bfc48721d57c0d1534de3684fb92e230cdcef
Nathan Dotz <nathan.dotz@gmail.com>
2012-03-16 21:44:58 -0400

    Have the server render analyze as a template, and inject json via sorcery

--- server/main.py
+++ server/main.py
@@ -1,6 +1,6 @@
 import wave, sys, struct, json, os
 
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 from werkzeug import secure_filename
 
 UPLOAD_FOLDER = '/tmp/'
@@ -16,23 +16,21 @@ def allowed_file(filename):
 
 @app.route('/')
 def main_page():
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="/upload" method=post enctype=multipart/form-data>
-      <p><input type="file" name="spikes_file">
-         <input type=submit value=Upload>
-    </form>
-    '''
+    return '''    '''
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    from jinja2 import FileSystemLoader
+    app.jinja_loader = FileSystemLoader(
+        os.path.join(os.path.dirname(__file__), '..'))
+
+
     file = request.files['spikes_file']
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('provide_json_of_wav', filename=filename))
+        return render_template('analyze.html',
+                               sampleData= provide_json_of_wav(filename))
     else:
         return "Invalid file"
 
@@ -44,9 +42,10 @@ def provide_json_of_wav(filename):
         for i in range(w.getnframes()):
             frame = w.readframes(1)
             pcm_list.append(struct.unpack("h", frame)[0])
-        return json.dumps(pcm_list[40:])
+        return json.dumps(pcm_list[40:4440])
     else:
-        return "Oops!, couldn't read" + os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        return "Oops!, couldn't read" + \
+                os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.config['DEBUG'] = True
