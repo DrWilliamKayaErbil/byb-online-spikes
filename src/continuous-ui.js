01b23c02316bbb24f431afd87bac697b74e89261
Nathan Dotz <nathan.dotz@gmail.com>
2012-05-31 16:43:17 -0400

    added ringbuffer

--- src/continuous-ui.js
+++ src/continuous-ui.js
@@ -1,12 +1,19 @@
-function passAudio(pcmData){
-  var pcmArr = new Array();
-  for(var i = 0; i < pcmData.length; i++){
-    pcmArr[i] = pcmData[i];
-  }
-  var wr ={
-    pcmdata: pcmArr
-  };
-  wr.pcmdata = pcmData;
-  BackyardBrains.analyze.setWaveData(wr);
-  BackyardBrains.analyze.redraw.trigger('redraw');
+rb = new RingBuffer(44100/2);
+rb.zeroFill();
+
+function redrawMyRingbuffer() {
+    var pcmArr = rb.getArray();
+    var wr ={
+        pcmdata: pcmArr
+    };
+    BackyardBrains.analyze.setWaveData(wr);
+    BackyardBrains.analyze.redraw.trigger('redraw');
 }
+
+function passAudio(pcmData){
+  for(var i = 0; i < pcmData.length; i++){
+    rb.addEnd(pcmData[i]);
+  }
+}
+
+var intervalID = setInterval(redrawMyRingbuffer, 33);
